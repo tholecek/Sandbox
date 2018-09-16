d3bcc768030bf05785f26df52a3412aacdd0a7a0
Thomas Holecek <tholecek@hotmail.com>
2018-09-16 14:25:00 -0400

    Add files via upload

---
+++ SPITEST.py
@@ -0,0 +1,26 @@
+import spidev
+import time
+
+delay = 0.1
+ldr_channel = 0
+
+#Create SPI
+spi = spidev.SpiDev()
+spi.open(0, 0)
+spi.max_speed_hz = 10000
+spi.mode = 0b11
+ 
+def readadc(adcnum):
+    # read SPI data from the MCP3008, 8 channels in total
+    if adcnum > 7 or adcnum < 0:
+        return -1
+    r = spi.xfer2([1, 8 + adcnum << 4, 0])
+    data = ((r[1] & 3) << 8) + r[2]
+    return data
+    
+ 
+while True:
+    ldr_value = readadc(ldr_channel)
+    print ("---------------------------------------")
+    print("LDR Value: %d" % ldr_value)
+    time.sleep(delay)
