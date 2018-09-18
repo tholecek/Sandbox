621b2c3a11bb510ec1181b055704a6abe97e3a3a
Thomas Holecek <tholecek@hotmail.com>
2018-09-18 14:53:40 -0400

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,3 @@
+import ReadMCP3008
+
+print("Channel 0 is ", ReadMCP3008.Test(0))
---
+++ ReadMCP3008.py
@@ -0,0 +1,23 @@
+#import spidev
+
+#spi = spidev.SpiDev()
+#spi.open(0, 0)
+#spi.max_speed_hz = 500000
+#spi.mode = 0b11
+print("I'm Here")
+
+def Read1Chan(n):
+    # read SPI data from the MCP3008, 8 channels in total
+    if n > 7 or n < 0:
+        return -1
+    r = spi.xfer2([1, 8 + n << 4, 0])
+    data = ((r[1] & 3) << 8) + r[2]
+    return data
+
+def Aread (channel):
+    output = Read1Chan(channel)
+    return output
+
+def Test (channel):
+    output = 1.0
+    return output
